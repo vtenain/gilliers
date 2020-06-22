1fa6e28199e90751befc1efad7be7f0c22d0b498
Vanessa Tenain <vanessa.tenain@gmail.com>
2020-06-22 15:48:18 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    loop:true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    mousewheel: true,
+  });
