8efb7ef1508569296b280b94c2919ddc7524555c
Vanessa Tenain <vanessa.tenain@gmail.com>
2020-06-24 09:43:13 +0200

    probleme diapo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-var swiper = new Swiper('.swiper-container', {
+var diapoAccueil = new Swiper('.diapoAccueil', {
     loop:true,
+    cssMode: true,
     pagination: {
       el: '.swiper-pagination',
+      clickable:true,
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-    mousewheel: true,
+  });
+  var diapoConfiance = new Swiper('.diapoConfiance', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      loop:true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
   });
