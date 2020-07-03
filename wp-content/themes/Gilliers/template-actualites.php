8f30016f80e6ecdded8f12ea77cbc3d16f8c40fd
Mateo Pommier <marteopommier.d7@gmail.com>
2020-07-03 11:57:13 +0200

    Ajout des col rsponsives

--- wp-content/themes/Gilliers/template-actualites.php
+++ wp-content/themes/Gilliers/template-actualites.php
@@ -29,114 +29,129 @@ Template Name: Page actualités
                 wp_reset_postdata();
             ?>
             <section class="cardsActus">
-                <section class="column">
+                <?php   
+                    $i=-1; $array = []; 
+                    $args = array ( 'post_type' => 'actualites', 'posts_per_page' => -1, 'orderby' => 'publish_date', 'order' => 'DESC');
+                    $the_query = new WP_Query( $args ); 
+                    if ($the_query->have_posts()): while ( $the_query->have_posts() ) : $the_query->the_post(); 
+                    $i++;
+                    array_push($array, [get_the_date(), get_the_title(), get_field("extrait")]); 
+                ?>
+                <?php endwhile; wp_reset_postdata();?>
+                <?php else: ?> <p>Désolé, aucun contenu à afficher</p>
+                <?php endif; ?>
+
+                <?php $col1 = []; $col2 = []; $col3 = []; $col4 = []; $col5 = []; $col6 = []; $col7 = []; $k=-1; 
+                for($j=0; $j < count($array); $j++):
+                    if($k === 3): $k = -1; endif; $k++;?>
+                    <?php if($k === 0): array_push($col1, $array[$j]); endif;?>
+                    <?php if($k === 1): array_push($col2, $array[$j]); endif;?>
+                    <?php if($k === 2): array_push($col3, $array[$j]); endif;?>
+                    <?php if($k === 3): array_push($col4, $array[$j]); endif;?>
+                <?php endfor; 
+
+                for($l=0; $l < count($array); $l++):
+                    if($k === 1): $k = -1; endif; $k++;?>
+                    <?php if($k === 0): array_push($col5, $array[$l]); endif;?>
+                    <?php if($k === 1): array_push($col6, $array[$l]); endif;?>
+                <?php endfor; 
+
+                for($m=0; $m < count($array); $m++): ?>
+                    <?php array_push($col7, $array[$m]);?>
+                <?php endfor; ?>
+
+                <section class="column large">
+                <?php for($a=0; $a < count($col1); $a++): ?>
                     <article>
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
                         <section>
-                            <p class="dateActu">Publiée le 10.01.2021</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                            <p class="dateActu"><?= $col1[$a][0] ?></p>
+                            <p class="titreActu"><?= $col1[$a][1] ?></p>
+                            <p class="descActu"><?= $col1[$a][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
                         </section>
                     </article>
-                    <article>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
-                        <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum </p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
-                        </section>
-                    </article>
-                    <article>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
-                        <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima. Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsa tenetur totam tempora perspiciatis officiis nobis iure! Veritatis, et autem, at eius praesentium voluptate debitis voluptas, quidem itaque exercitationem adipisci.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
-                        </section>
-                    </article>
+                <?php endfor; ?>
                 </section>
-                
-                <section class="column">
+                <section class="column large">
+                <?php for($b=0; $b < count($col2); $b++): ?>
                     <article>
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
                         <section>
-                            <p class="dateActu">Publiée le 10.01.2022</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                            <p class="dateActu"><?= $col2[$b][0] ?></p>
+                            <p class="titreActu"><?= $col2[$b][1] ?></p>
+                            <p class="descActu"><?= $col2[$b][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
                         </section>
                     </article>
-                    <article>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
-                        <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
-                        </section>
-                    </article>
-                    <article>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
-                        <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
-                        </section>
-                    </article>
+                <?php endfor; ?>
                 </section>
-                
-                <section class="column">
-                    <article>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualité">
-                        <section>
-                            <p class="dateActu">Publiée le 10.01.2023</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
-                        </section>
-                    </article>
+                <section class="column large">
+                <?php for($c=0; $c < count($col3); $c++): ?>
                     <article>
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
                         <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur suscipit harum nulla a nesciunt alias sed odio voluptatibus, reiciendis pariatur quas modi expedita eveniet facilis sunt iste, culpa beatae! Perferendis?</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                            <p class="dateActu"><?= $col3[$c][0] ?></p>
+                            <p class="titreActu"><?= $col3[$c][1] ?></p>
+                            <p class="descActu"><?= $col3[$c][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
                         </section>
-                    </article>  
+                    </article>
+                <?php endfor; ?>
                 </section>
-                <section class="column">
+                <section class="column large">
+                <?php for($d=0; $d < count($col4); $d++): ?>
                     <article>
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
                         <section>
-                            <p class="dateActu">Publiée le 10.01.2022</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                            <p class="dateActu"><?= $col4[$d][0] ?></p>
+                            <p class="titreActu"><?= $col4[$d][1] ?></p>
+                            <p class="descActu"><?= $col4[$d][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
                         </section>
                     </article>
+                <?php endfor; ?>
+                </section>
+
+                <section class="column medium">
+                <?php for($e=0; $e < count($col5); $e++): ?>
                     <article>
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
                         <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                            <p class="dateActu"><?= $col5[$e][0] ?></p>
+                            <p class="titreActu"><?= $col5[$e][1] ?></p>
+                            <p class="descActu"><?= $col5[$e][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
                         </section>
                     </article>
+                <?php endfor; ?>
+                </section>
+                <section class="column medium">
+                <?php for($f=0; $f < count($col6); $f++): ?>
                     <article>
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
                         <section>
-                            <p class="dateActu">Publiée le 10.01.2020</p>
-                            <p class="titreActu">Lorem ipsum dolor sit amet</p>
-                            <p class="descActu">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus similique, enim ad totam quos nam ducimus minima.</p>
-                            <p class="btn-actu" style="background:url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-btn.png') no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                            <p class="dateActu"><?= $col6[$f][0] ?></p>
+                            <p class="titreActu"><?= $col6[$f][1] ?></p>
+                            <p class="descActu"><?= $col6[$f][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
                         </section>
                     </article>
+                <?php endfor; ?>
+                </section>
+
+                <section class="column small">
+                <?php for($g=0; $g < count($col7); $g++): ?>
+                    <article>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actus.png" alt="Photo actualtité">
+                        <section>
+                            <p class="dateActu"><?= $col7[$g][0] ?></p>
+                            <p class="titreActu"><?= $col7[$g][1] ?></p>
+                            <p class="descActu"><?= $col7[$g][2] ?></p>
+                            <p class="btn-actu" style="background:url("<?php echo get_stylesheet_directory_uri(); ?>"/assets/img/bg-btn.png") no-repeat; background-size:cover; background-position:center;"><a href="#">En savoir plus</a></p>
+                        </section>
+                    </article>
+                <?php endfor; ?>
                 </section>
             </section>
         </section>
